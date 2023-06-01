6b8f57491843ad20caf02a58d0015eefaf794186
Brandt Carlson <brandt.carlson11@gmail.com>
2023-06-01 16:08:31 -0500

    deets in state, next steps dynamic buttons

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,12 +5,14 @@ const About = () => {
     return (
         <>
             <h1>Me!</h1>
-
+            <p className="photo-cap">This isn't me but it IS an image</p>
+            <p className="photo-cap"> generated with prompts that could loosely relate to me. . .</p>
+            <img id='jh' src="./src/assets/JH_ai.jpg" ></img>
             <h2>You could say a lot about me</h2>
             <h3>Are they saying a lot about me?</h3>
             <p>That kid who took apart the remote, game-controller, cd-player (showing age now huh?), bike, car and more: that was me!  At least half of those things got put back together and a couple were even improved!</p>
             <p>Now I've taken that same success ratio and applied it to software and apps.  Watch out internet, some small portions of you are due for improvement. . . </p>
-            <h6>Did you come here to offer me untold amounts of money for a job?  Sweet, I'm glad you're here and you should get in touch (links below!).  If not, that's ok, we can still hang (links below!)</h6>
+            <h6>Did you come here to offer me untold amounts of money for a job?  Sweet, I'm glad you're here and you should get in touch (links below!).  If not, that's ok, we can still hang (same links below!)</h6>
             <div>
             <Link class='logo-link' to='https://www.linkedin.com/in/brandt-carlson-51735625' target="_blank"><img src='./src/assets/In-White-48.png'/></Link>
             <Link class='logo-link' to='https://github.com/Brandt-C' target="_blank"><img src="./src/assets/github-mark-white.png" style={{ maxWidth: 50 + 'px'}}/></Link>
--- src/views/Mad.jsx
+++ src/views/Mad.jsx
@@ -3,10 +3,17 @@ import axios from "axios";
 import CharButton from "../componenets/CharButton";
 import LocButton from "../componenets/LocButton";
 
+import SnowshoeingIcon from '@mui/icons-material/Snowshoeing';
+import { Button } from '@mui/material';
+
+
+        // <Button variant='oulined' startIcon={<SnowshoeingIcon />}>Bring in Characters</Button>
+
+// Going to have to bring in buttons here for usability's sake and trash components?  
+
 const Mad = () => {
 
     // const [story, setStory] = useState(null);
-    const [deets, setDeets] = useState();
 
     const getDeets = async () => {
         let response = await axios.get('http://127.0.0.1:5000/api/story/rando');
@@ -14,12 +21,15 @@ const Mad = () => {
     };
     const loadDeets = async () => {
         let data = await getDeets();
-        console.log(data)
+        console.log(data);
+        setDeets(data)
     }
-    loadDeets();
+    const [deets, setDeets] = useState(()=> loadDeets());
+    ;
 
     return (
         <>
+            
             <h1>Under construction!</h1>
             <h3>This is gonna be some really cool stuff.  Pretty soon.  I swear.</h3>
             <h4>Like a mad-lib generator with choices from multiple fictional universes.</h4>
