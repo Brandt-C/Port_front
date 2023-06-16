e9defe09ba9fbf8c4ac55185275649ded0e9f964
Brandt Carlson <brandt.carlson11@gmail.com>
2023-06-16 10:12:51 -0500

    little more func in story

--- src/views/Story.jsx
+++ src/views/Story.jsx
@@ -1,7 +1,9 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import CharCard from "../components/CharCard";
 import "../css/story.css"
 import LocCard from "../components/LocCard";
+
+import { Button } from "@mui/material";
 
 const Story = () => {
 
@@ -30,11 +32,16 @@ const Story = () => {
                     })}
                 </div>
 
-                <div className="story container">
+                <div className="story-container">
                     {state.text.map((ptag, key) => {
                         return <p key={key} className="text">{ptag}</p>
                     })}
                     
+                </div>
+                <div className="story-action-container">
+                    <Button sx={{ mx:3 }} variant="outlined" color="warning" onClick={print}>Print/save</Button>
+                   <Link to='/mad'><Button sx={{ mx:3 }} variant="outlined">new story</Button></Link>
+                   {/* <Link to='/mad'><Button variant='outlined' color='danger'>Create new story</Button></Link>  */}
                 </div>
 
             </div>
